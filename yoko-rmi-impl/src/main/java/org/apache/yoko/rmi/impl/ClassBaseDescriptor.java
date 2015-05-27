e130c215141bb9874702cf8054f4a3c1f46124da
Neil Richards <ngmr@linux.vnet.ibm.com>
2015-05-27 13:32:00 +0100

    Use doPrivileged in ClassBaseDescriptor.init()

--- yoko-rmi-impl/src/main/java/org/apache/yoko/rmi/impl/ClassBaseDescriptor.java
+++ yoko-rmi-impl/src/main/java/org/apache/yoko/rmi/impl/ClassBaseDescriptor.java
@@ -1,12 +1,14 @@
 package org.apache.yoko.rmi.impl;
 
 import java.lang.reflect.Field;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 
 import javax.rmi.CORBA.ClassDesc;
 
 import org.omg.CORBA.MARSHAL;
 
-public abstract class ClassBaseDescriptor extends ValueDescriptor {
+abstract class ClassBaseDescriptor extends ValueDescriptor {
 
     ClassBaseDescriptor(Class type, TypeRepository repository) {
         super(type, repository);
@@ -16,16 +18,23 @@ public abstract class ClassBaseDescriptor extends ValueDescriptor {
     public void init() {
         super.init();
 
-        Class<?> clz = ClassDesc.class;
-        try {
-            final Field repid_field = clz.getDeclaredField("repid");
-            repid_field.setAccessible(true);
-            final Field codebase_field = clz.getDeclaredField("codebase");
-            codebase_field.setAccessible(true);
-            init(repid_field, codebase_field);
-        } catch (NoSuchFieldException ex) {
-            throw new MARSHAL("no such field: " + ex);
-        }
+        final Class<?> clz = ClassDesc.class;
+
+        AccessController.doPrivileged(new PrivilegedAction<Void>() {
+            @Override
+            public Void run() {
+                try {
+                    final Field repid_field = clz.getDeclaredField("repid");
+                    repid_field.setAccessible(true);
+                    final Field codebase_field = clz.getDeclaredField("codebase");
+                    codebase_field.setAccessible(true);
+                    init(repid_field, codebase_field);
+                } catch (NoSuchFieldException ex) {
+                    throw (MARSHAL)new MARSHAL("no such field: " + ex).initCause(ex);
+                }
+                return null;
+            }
+        });
     }
 
     abstract void init(Field repid_field, Field codebase_field);
