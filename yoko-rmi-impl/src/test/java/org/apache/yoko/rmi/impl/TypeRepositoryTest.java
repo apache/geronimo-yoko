30b7a5bed1b6700cd9f319c65ccd48f1fa0c100c
Joe Chacko <143064+chackoj@users.noreply.github.com>
2019-08-05 13:14:21 +0100

    Create multi-threaded repository test

---
+++ yoko-rmi-impl/src/test/java/org/apache/yoko/rmi/impl/TypeRepositoryTest.java
@@ -0,0 +1,108 @@
+package org.apache.yoko.rmi.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.io.Serializable;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
+import static java.util.stream.IntStream.range;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+public class TypeRepositoryTest {
+    TypeRepository repo;
+
+    @BeforeEach
+    public void setup() {
+        this.repo = TypeRepository.get();
+    }
+
+    @Test
+    public void testTypeRepository() {
+        class X implements Serializable {
+            private static final long serialVersionUID = 0xDEADBEEF0FACEF00L;
+        }
+        final TypeDescriptor descriptor = repo.getDescriptor(X.class);
+        assertThat(descriptor.getRepositoryID(), is(equalTo("RMI:org.apache.yoko.rmi.impl.TypeRepositoryTest\\U00241X:56D0CD61110C30C5:DEADBEEF0FACEF00")));
+    }
+
+    @Test
+    public void testTypeRepositoryMultiThreaded() throws Exception {
+        class X implements Serializable {
+            private static final long serialVersionUID = 0xDEADBEEF0FACEF00L;
+            byte b;
+            char c;
+            double d;
+            float f;
+            int i;
+            long l;
+        }
+        final String expectedTypeInfo = getTypeRepoInfo(X.class, 1).iterator().next();
+        range(0, 1024).forEach(unused -> {
+            Set<String> typeInfos = getTypeRepoInfo(new OverridingLoader().loadClass(X.class.getName()), Runtime.getRuntime().availableProcessors());
+            for (String info: typeInfos) {
+                assertThat(info, is(expectedTypeInfo));
+            }
+            assertThat(typeInfos, contains(expectedTypeInfo));
+        });
+    }
+
+    public Set<String> getTypeRepoInfo(Class type, int numThreads) {
+        CountDownLatch latch = new CountDownLatch(numThreads);
+        ForkJoinPool pool = new ForkJoinPool(numThreads);
+        try {
+            return range(0, numThreads)
+                .mapToObj(i -> pool.submit(() -> {
+                    try {
+                        latch.await();
+                    } catch (InterruptedException e) { }
+                    final ValueDescriptor descriptor = (ValueDescriptor)repo.getDescriptor(type);
+                    StringBuilder sb = new StringBuilder();
+                    for (FieldDescriptor fd: descriptor._fields) {
+                        final String fType = fd == null ? "null" : fd.getType().getName();
+                        final String fName = fd == null ? "null" : fd.getIDLName();
+                        sb.append(String.format("%-20s ", fType + ' ' + fName + ';'));
+                    }
+                    return sb.toString();
+                }))
+                .peek(dummy -> latch.countDown())
+                .collect(toList()).stream()
+                .map(ForkJoinTask::join)
+                .collect(toSet());
+        } finally {
+            pool.shutdown();
+        }
+    }
+
+    private static class OverridingLoader extends ClassLoader {
+        OverridingLoader() {
+            super(OverridingLoader.class.getClassLoader());
+        }
+
+        @Override
+        public Class<?> loadClass(String name) {
+            try {
+                if (name.startsWith(TypeRepositoryTest.class.getName() + "$")) {
+                    // get the bytes from the parent loader
+                    InputStream in = getResourceAsStream(name.replace('.', '/') + ".class");
+                    byte[] bytes = new byte[in.available()];
+                    in.read(bytes);
+                    // define the class locally and return it
+                    return defineClass(name, bytes, 0, bytes.length);
+                }
+                return super.loadClass(name);
+            } catch (Exception e) {
+                throw (NoClassDefFoundError) new NoClassDefFoundError("Caught IOException").initCause(e);
+            }
+        }
+    }
+}
