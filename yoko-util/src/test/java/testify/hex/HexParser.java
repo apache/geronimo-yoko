3db0288817afc5f44f3532730c78a4380c5560e0
Joe Chacko <143064+chackoj@users.noreply.github.com>
2019-11-05 13:47:30 +0000

    Clarify HexParser test utility

--- yoko-util/src/test/java/testify/hex/HexParser.java
+++ yoko-util/src/test/java/testify/hex/HexParser.java
@@ -7,61 +7,13 @@ import java.io.OutputStream;
 import java.io.StringReader;
 import java.math.BigInteger;
 import java.text.ParseException;
+import java.util.Arrays;
 import java.util.regex.Pattern;
 
 import static java.lang.Character.toUpperCase;
 
 public enum HexParser {
     HEX_DUMP {
-        private final String COMMENT = "--";
-        private final Pattern HEX_LINE = Pattern.compile("([0-9 a-f]{8} ){4} \".{1,16}\"");
-        private final byte[] HI_NYBL = new byte['g'];
-        private final byte[] LO_NYBL = new byte['g'];
-        {
-            char[] hexChars = "0123456789abcdef".toCharArray();
-            for (byte i = 0; i < hexChars.length; i++) {
-                LO_NYBL[toUpperCase(hexChars[i])] = LO_NYBL[hexChars[i]] = i;
-                HI_NYBL[toUpperCase(hexChars[i])] = HI_NYBL[hexChars[i]] = (byte)(i << 4);
-            }
-        }
-
-        class InnerParser {
-            final OutputStream out;
-            final char[] chars;
-            int byteIndex = 0;
-
-            private InnerParser(OutputStream out, char[] chars) {
-                this.out = out;
-                this.chars = chars;
-            }
-
-            private boolean hasAnyBytesLeft() {
-                // skip any spaces in the hex
-                for (int h = byteIndex * 2 + byteIndex / 4; chars[h] == ' ' && byteIndex <= 16; byteIndex++);
-                return byteIndex < 16;
-            }
-
-            private void writeNextByte() throws IOException, ParseException {
-                byte b = getByte();
-                char c = getChar();
-
-                // sanity check ascii string
-                if (c == '.' || c == b) {
-                    out.write(b);
-                    byteIndex++;
-                } else {
-                    String msg = "Invalid ascii character '%s' when parsing byte %02x at index %d";
-                    msg = String.format(msg, c, b, byteIndex);
-                    throw new ParseException(msg, byteIndex);
-                }
-            }
-
-            private byte highNybble() {return HI_NYBL[chars[byteIndex * 2 + byteIndex / 4]];}
-            private byte lowNybble() {return LO_NYBL[chars[1 + byteIndex * 2 + byteIndex / 4]];}
-            private byte getByte() {return (byte)(highNybble()|lowNybble());}
-            private char getChar() {return chars[byteIndex+38];}
-        }
-
         public byte[] parse(final String text) {
             if (text == null) return null;
             StringReader sr = new StringReader(text);
@@ -72,23 +24,16 @@ public enum HexParser {
                 while (null != (line = br.readLine())) {
                     line = line.trim();
                     lineNo++;
-                    if (line.isEmpty()) {
-                        // ignore blank lines
-                    } else if (line.startsWith(COMMENT)) {
-                        // ignore comment lines
-                    } else if (HEX_LINE.matcher(line).matches()) {
-                        InnerParser ip = new InnerParser(out, line.toCharArray());
-                        while (ip.hasAnyBytesLeft()) ip.writeNextByte();
-                    } else {
-                        throw new IllegalArgumentException("Could not parse as hex dump: line " + lineNo + " of text:\n" + text );
-                    }
+                    HexLineParser lineParser = HexLineParser.parse(line);
+                    if (lineParser == null) continue;
+                    lineParser.writeBytes(out);
                 }
 
                 return out.toByteArray();
             } catch (IOException e) {
                 throw new Error(e);
             } catch (ParseException e) {
-                throw new IllegalArgumentException("Could not parse as hex dump: line " + lineNo + " of text:\n" + text, e);
+                throw new IllegalArgumentException("Could not parse as hex dump: line " + lineNo + "\n", e);
             }
         }
     },
@@ -119,6 +64,71 @@ public enum HexParser {
             return nBytes;
         }
     };
+
+    private static class HexLineParser {
+        private static final String COMMENT = "--";
+        private static final Pattern HEX_LINE = Pattern.compile("([0-9 a-f]{8} ){4} \".{1,16}\"");
+        private static final char[] HEX_CHARS = "0123456789abcdef".toCharArray();
+        static { Arrays.sort(HEX_CHARS); }
+        private static final byte[] HI_NYBL = new byte['g'];
+        private static final byte[] LO_NYBL = new byte['g'];
+        static {
+            char[] hexChars = "0123456789abcdef".toCharArray();
+            for (byte i = 0; i < hexChars.length; i++) {
+                LO_NYBL[toUpperCase(hexChars[i])] = LO_NYBL[hexChars[i]] = i;
+                HI_NYBL[toUpperCase(hexChars[i])] = HI_NYBL[hexChars[i]] = (byte)(i << 4);
+            }
+        }
+
+        static HexLineParser parse(String line) throws ParseException {
+            if (line.isEmpty()) {
+                // ignore blank lines
+                return null;
+            } else if (line.startsWith(COMMENT)) {
+                // ignore comment lines
+                return null;
+            } else if (HEX_LINE.matcher(line).matches()) {
+                return new HexLineParser(line);
+            } else {
+                throw new ParseException(line, 0);
+            }
+        }
+
+        final char[] chars;
+        int logicalByteIndex = 0;
+
+        private HexLineParser(String line) {
+            this.chars = line.toCharArray();
+        }
+
+        void writeBytes(OutputStream out) throws IOException, ParseException { while(hasAnyBytesLeft()) writeNextByte(out);}
+
+        private boolean hasAnyBytesLeft() {
+            // true iff we haven't read 16 bytes yet AND there isn't a space under the cursor
+            return logicalByteIndex < 16 && chars[cursor()] != ' ';
+        }
+
+        private void writeNextByte(OutputStream out) throws IOException, ParseException {
+            byte b = getByte();
+            char c = getAsciiChar();
+
+            // sanity check ascii string
+            if (c == '.' || c == b) {
+                out.write(b);
+                logicalByteIndex++;
+            } else {
+                String msg = "Invalid ascii character '%s' when parsing byte %02x at index %d";
+                msg = String.format(msg, c, b, logicalByteIndex);
+                throw new ParseException(msg, logicalByteIndex);
+            }
+        }
+
+        private int cursor() { return logicalByteIndex * 2 + logicalByteIndex / 4; }
+        private byte hiNybble() { return HI_NYBL[chars[cursor()]];}
+        private byte loNybble() { return LO_NYBL[chars[cursor() + 1]]; }
+        private byte getByte() { return (byte)(hiNybble() | loNybble()); }
+        private char getAsciiChar() { return chars[logicalByteIndex + 38]; }
+    }
 
     abstract public byte[] parse(final String text);
 }
