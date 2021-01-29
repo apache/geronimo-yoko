2abfbdc182bb9242d076ea2d632b0714ebe76c16
Joe Chacko <143064+chackoj@users.noreply.github.com>
2021-01-29 09:46:42 +0000

    Test multi-threaded client on server restart

--- yoko-core/src/test/java/org/apache/yoko/ServerRestartTest.java
+++ yoko-core/src/test/java/org/apache/yoko/ServerRestartTest.java
@@ -18,6 +18,10 @@ package org.apache.yoko;
 
 import acme.Echo;
 import acme.EchoImpl;
+import org.apache.yoko.orb.OCI.Buffer;
+import org.apache.yoko.util.HexConverter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.omg.CORBA.ORB;
 import org.omg.CosNaming.NameComponent;
@@ -33,7 +37,15 @@ import testify.jupiter.annotation.iiop.ServerControl;
 import testify.util.Stubs;
 
 import java.rmi.RemoteException;
+import java.util.Collection;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+import static java.util.stream.Collectors.joining;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -102,5 +114,42 @@ public class ServerRestartTest {
         ctx1.bind_new_context(new NameComponent[]{new NameComponent("wibble", "")});
         // force the other stub to reconnect
         ctx2.bind_new_context(new NameComponent[]{new NameComponent("splong", "")});
+    }
+
+    /** Test a single thread calling the server from naming lookup to invocation */
+    @Test
+    public void testMultipleThreads(ORB clientOrb) throws Exception {
+        assertNotNull(stubUrl);
+        final int parallelism = 2;
+        String actual  = new ForkJoinPool(parallelism + 1)
+                .submit( () -> IntStream.range(0, parallelism)
+                        .mapToObj(Integer::toString)
+                        .parallel()
+                        .map(expected -> {
+                            System.out.println("Starting task " + expected);
+                            Echo echo = Stubs.toStub(ServerRestartTest.stubUrl, clientOrb, Echo.class);
+                            String result;
+                            try { result = echo.echo(expected); } catch (RemoteException e) { throw (Error)Assertions.fail(e); }
+                            assertEquals(expected, result, Thread.currentThread().getName() + " should successfully look up and invoke the echo object");
+                            return result;
+                        }).sorted().collect(joining(""))
+                ).get();
+        System.out.println(actual);
+        String expected = IntStream.range(0, parallelism).mapToObj(Integer::toString).sorted().collect(joining(""));
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Test the restart behaviour when the client is multi-threaded.
+     * Since this is non-deterministic, we repeat this test enough times to be confident of seeing significant failures.
+     * @param clientOrb
+     * @throws Exception
+     */
+    @Test
+    @RepeatedTest(50)
+    public void testMultipleThreadsAcrossRestart(ORB clientOrb) throws Exception {
+        testMultipleThreads(clientOrb);
+        serverControl.restart();
+        testMultipleThreads(clientOrb);
     }
 }
