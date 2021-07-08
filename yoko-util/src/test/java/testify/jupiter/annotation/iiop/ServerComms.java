f2093710d5ba290ec198683cf0308316337d86d5
Joe Chacko <143064+chackoj@users.noreply.github.com>
2021-07-08 17:16:00 +0100

    TFW: wrap all ServerComms errors to add detail

--- yoko-util/src/test/java/testify/jupiter/annotation/iiop/ServerComms.java
+++ yoko-util/src/test/java/testify/jupiter/annotation/iiop/ServerComms.java
@@ -399,11 +399,15 @@ final class ServerComms implements Serializable {
         assertClientSide();
         final String requestId = getNextRequestId();
         bus.log("Waiting for completion of " + requestId);
-        String info = bus.get(requestId);
-        bus.log(requestId + ": " + info);
-        final ServerSideException result = bus.get(Result.RESULT);
-        if (result == null) return;
-        throw wrapper.apply(result.resolve());
+        try {
+            String info = bus.get(requestId);
+            bus.log(requestId + ": " + info);
+            final ServerSideException result = bus.get(Result.RESULT);
+            if (result == null) return;
+            throw result.resolve();
+        } catch (Throwable t) {
+            throw wrapper.apply(t);
+        }
     }
 
     private <T> void completeRequest(String prefix, Consumer<T> consumer, T parameter) {
