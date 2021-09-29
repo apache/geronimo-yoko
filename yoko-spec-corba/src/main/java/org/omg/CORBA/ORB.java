f1b374ea5b3cc295f03639ab08f68db4d62b2eb0
Neil Richards <7203556+ngmr@users.noreply.github.com>
2021-09-29 11:35:53 +0100

    DEV: simplify doPrivileged() in corba-spec

--- yoko-spec-corba/src/main/java/org/omg/CORBA/ORB.java
+++ yoko-spec-corba/src/main/java/org/omg/CORBA/ORB.java
@@ -17,17 +17,25 @@
 package org.omg.CORBA;
 
 import org.apache.yoko.osgi.ProviderLocator;
+import org.omg.CORBA.ORBPackage.InvalidName;
+import org.omg.CORBA.portable.OutputStream;
 
+import java.applet.Applet;
 import java.security.PrivilegedAction;
-import java.security.AccessController;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
+import java.util.Objects;
 import java.util.Properties;
+
+import static java.lang.Thread.currentThread;
+import static java.security.AccessController.doPrivileged;
+import static org.omg.CORBA.ORB.OrbSingletonHolder.ORB_SINGLETON;
 
 public abstract class ORB {
 
     public abstract String[] list_initial_services();
 
-    public abstract org.omg.CORBA.Object resolve_initial_references(
-            String object_name) throws org.omg.CORBA.ORBPackage.InvalidName;
+    public abstract org.omg.CORBA.Object resolve_initial_references(String object_name) throws InvalidName;
 
     public abstract String object_to_string(org.omg.CORBA.Object object);
 
@@ -60,12 +68,10 @@ public abstract class ORB {
 
     public abstract boolean poll_next_response();
 
-    public abstract Request get_next_response()
-            throws org.omg.CORBA.WrongTransaction;
+    public abstract Request get_next_response() throws WrongTransaction;
 
-    public boolean get_service_information(short service_type,
-            ServiceInformationHolder service_info) {
-        throw new org.omg.CORBA.NO_IMPLEMENT();
+    public boolean get_service_information(short service_type, ServiceInformationHolder service_info) {
+        throw new NO_IMPLEMENT();
     }
 
     public abstract TypeCode create_struct_tc(String id, String name,
@@ -90,7 +96,7 @@ public abstract class ORB {
     public abstract TypeCode create_wstring_tc(int bound);
 
     public TypeCode create_fixed_tc(short digits, short scale) {
-        throw new org.omg.CORBA.NO_IMPLEMENT();
+        throw new NO_IMPLEMENT();
     }
 
     public abstract TypeCode create_sequence_tc(int bound, TypeCode elementType);
@@ -102,53 +108,51 @@ public abstract class ORB {
 
     public abstract TypeCode create_array_tc(int length, TypeCode elementType);
 
-    public TypeCode create_value_tc(String id, String name,
-            short type_modifier, TypeCode concrete_base, ValueMember[] members) {
-        throw new org.omg.CORBA.NO_IMPLEMENT();
+    public TypeCode create_value_tc(String id, String name, short type_modifier, TypeCode concrete_base, ValueMember[] members) {
+        throw new NO_IMPLEMENT();
     }
 
-    public TypeCode create_value_box_tc(String id, String name,
-            TypeCode boxed_type) {
-        throw new org.omg.CORBA.NO_IMPLEMENT();
+    public TypeCode create_value_box_tc(String id, String name, TypeCode boxed_type) {
+        throw new NO_IMPLEMENT();
     }
 
     public TypeCode create_native_tc(String id, String name) {
-        throw new org.omg.CORBA.NO_IMPLEMENT();
+        throw new NO_IMPLEMENT();
     }
 
     public TypeCode create_recursive_tc(String id) {
-        throw new org.omg.CORBA.NO_IMPLEMENT();
+        throw new NO_IMPLEMENT();
     }
 
     public TypeCode create_abstract_interface_tc(String id, String name) {
-        throw new org.omg.CORBA.NO_IMPLEMENT();
+        throw new NO_IMPLEMENT();
     }
 
     public abstract TypeCode get_primitive_tc(TCKind kind);
 
     public boolean work_pending() {
-        throw new org.omg.CORBA.NO_IMPLEMENT();
+        throw new NO_IMPLEMENT();
     }
 
     public void perform_work() {
-        throw new org.omg.CORBA.NO_IMPLEMENT();
+        throw new NO_IMPLEMENT();
     }
 
     public void run() {
-        throw new org.omg.CORBA.NO_IMPLEMENT();
+        throw new NO_IMPLEMENT();
     }
 
     public void shutdown(boolean wait_for_completion) {
-        throw new org.omg.CORBA.NO_IMPLEMENT();
+        throw new NO_IMPLEMENT();
     }
 
     public void destroy() {
-        throw new org.omg.CORBA.NO_IMPLEMENT();
+        throw new NO_IMPLEMENT();
     }
 
     public abstract Any create_any();
 
-    public abstract org.omg.CORBA.portable.OutputStream create_output_stream();
+    public abstract OutputStream create_output_stream();
 
     // Empty method for binary compatibility with the 1.5
     public void connect(org.omg.CORBA.Object obj) {};
@@ -156,23 +160,26 @@ public abstract class ORB {
     // Empty method for binary compatibility with the 1.5
     public void disconnect(org.omg.CORBA.Object obj) {};
 
-    public org.omg.CORBA.Policy create_policy(int policy_type,
-            org.omg.CORBA.Any val) throws org.omg.CORBA.PolicyError {
-        throw new org.omg.CORBA.NO_IMPLEMENT();
+    public Policy create_policy(int policy_type, Any val) throws PolicyError {
+        throw new NO_IMPLEMENT();
     }
 
     /**
      * @deprecated Deprecated by CORBA 2.2.
      */
     public Current get_current() {
-        throw new org.omg.CORBA.NO_IMPLEMENT();
+        throw new NO_IMPLEMENT();
     }
 
-    private static ORB ORBSingleton_;
     private static final String ORBClassPropertyKey = "org.omg.CORBA.ORBClass";
     private static final String ORBSingletonPropertyKey = "org.omg.CORBA.ORBSingletonClass";
 
-    public static ORB init(String[] args, java.util.Properties props) {
+    enum OrbSingletonHolder {
+        ;
+        static final ORB ORB_SINGLETON = createSingletonOrb();
+    }
+
+    public static ORB init(String[] args, Properties props) {
 
         ORB orb = newOrb(props);
 
@@ -181,7 +188,7 @@ public abstract class ORB {
         return orb;
     }
 
-    public static ORB init(java.applet.Applet app, java.util.Properties props) {
+    public static ORB init(Applet app, Properties props) {
         ORB orb = newOrb(props);
 
         orb.set_parameters(app, props);
@@ -190,101 +197,45 @@ public abstract class ORB {
     }
 
     private static ORB newOrb(Properties props) {
-        String orbClassName = null;
-
-        if (props != null) {
-            orbClassName = props.getProperty(ORBClassPropertyKey);
-        }
-
-        ORB orb = null;
-        if (orbClassName == null) {
-            try {
-                orb = (ORB) ProviderLocator.getService(ORBClassPropertyKey, ORB.class, Thread.currentThread().getContextClassLoader());
-            } catch (Exception ex) {
-                throw (INITIALIZE)new INITIALIZE("Invalid ORB class from osgi: ").initCause(ex);
-            }
-        }
-
-        if (orb == null) {
-            try {
-                if (orbClassName == null)
-                    orbClassName = getSystemProperty(ORBClassPropertyKey);
-            } catch (SecurityException ex) {
-                // ignore
-            }
-
-            if (orbClassName == null)
-                orbClassName = "org.apache.yoko.orb.CORBA.ORB";
-
-
-            try {
-                // get the appropriate class for the loading.
-                ClassLoader loader = Thread.currentThread().getContextClassLoader();
-                orb = (ORB) ProviderLocator.loadClass(orbClassName, ORB.class, loader).newInstance();
-            } catch (Throwable ex) {
-                throw (INITIALIZE)new INITIALIZE("Invalid ORB class: "
-                        + orbClassName).initCause(ex);
-            }
-        }
-        return orb;
+        return newOrb(ORBClassPropertyKey, (null == props) ? null : props.getProperty(ORBClassPropertyKey));
     }
+
+    private static ORB createSingletonOrb() {
+        return newOrb(ORBSingletonPropertyKey, null);
+    }
+
+    private static ORB newOrb(String propertyKey, String orbClassName) {
+        final ClassLoader contextClassLoader = doPriv(currentThread()::getContextClassLoader);
+
+        if (null == orbClassName) {
+            try {
+                return Objects.requireNonNull((ORB)ProviderLocator.getService(propertyKey, ORB.class, contextClassLoader));
+            } catch (NullPointerException ignored) { // ORB not found, but without exception
+            } catch (Exception ex) {
+                throw (INITIALIZE)new INITIALIZE(String.format("Invalid %s class from osgi: ",propertyKey)).initCause(ex);
+            }
+            orbClassName = doPriv(() -> System.getProperty(propertyKey, "org.apache.yoko.orb.CORBA.ORB"));
+        }
+
+        try {
+            final Class<? extends ORB> orbClass = ProviderLocator.loadClass(orbClassName, ORB.class, contextClassLoader);
+            return doPrivEx(orbClass::getConstructor).newInstance();
+        } catch (Throwable ex) {
+            throw (INITIALIZE)new INITIALIZE(String.format("Invalid %s class: %s", propertyKey, orbClassName)).initCause(ex);
+        }
+    }
+
+    private static <T> T doPriv(PrivilegedAction<T> action) { return doPrivileged(action); }
+    private static <T> T doPrivEx(PrivilegedExceptionAction<T> action) throws PrivilegedActionException { return doPrivileged(action); }
+
 
     public static ORB init() {
-        if (ORBSingleton_ == null) {
-            try {
-                ORBSingleton_ = (ORB) ProviderLocator.getService(ORBSingletonPropertyKey, ORB.class, Thread.currentThread().getContextClassLoader());
-            } catch (Exception ex) {
-                throw (org.omg.CORBA.INITIALIZE)new org.omg.CORBA.INITIALIZE(
-                        "Invalid ORB singleton class from osgi: ").initCause(ex);
-
-            }
-            if (ORBSingleton_ == null) {
-                String orbClassName = getSystemProperty(ORBSingletonPropertyKey);
-
-                if (orbClassName == null) {
-                    orbClassName = "org.apache.yoko.orb.CORBA.ORBSingleton";
-                }
-
-                try {
-                    ORBSingleton_ = (ORB) ProviderLocator.loadClass(orbClassName, ORB.class, Thread.currentThread().getContextClassLoader()).newInstance();
-                } catch (ClassNotFoundException ex) {
-                    throw (org.omg.CORBA.INITIALIZE)new org.omg.CORBA.INITIALIZE(
-                            "Invalid ORB singleton class from osgi: ").initCause(ex);
-                } catch (InstantiationException ex) {
-                    throw (org.omg.CORBA.INITIALIZE)new org.omg.CORBA.INITIALIZE(
-                            "Invalid ORB singleton class from osgi: ").initCause(ex);
-                } catch (IllegalAccessException ex) {
-                    throw (org.omg.CORBA.INITIALIZE)new org.omg.CORBA.INITIALIZE(
-                            "Invalid ORB singleton class from osgi: ").initCause(ex);
-                }
-            }
-        }
-
-        return ORBSingleton_;
+        return ORB_SINGLETON;
     }
 
-    protected abstract void set_parameters(String[] args,
-            java.util.Properties props);
+    protected abstract void set_parameters(String[] args, Properties props);
 
-    protected abstract void set_parameters(java.applet.Applet app,
-            java.util.Properties props);
+    protected abstract void set_parameters(Applet app, Properties props);
 
 
-    /**
-     * Simple utility for retrieving a system property
-     * using the AccessController.
-     *
-     * @param name   The property name
-     *
-     * @return The property value.
-     */
-    private static String getSystemProperty(final String name) {
-	    return (String) AccessController.doPrivileged(
-                new PrivilegedAction() {
-                    public java.lang.Object run() {
-                        return System.getProperty(name);
-                    }
-                }
-		    );
-    }
 }
