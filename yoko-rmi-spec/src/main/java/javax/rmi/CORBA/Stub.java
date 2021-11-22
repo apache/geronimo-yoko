39ff3612235a6f3da8fe6b13f97ddd2cdce47b71
Joe Chacko <143064+joe-chacko@users.noreply.github.com>
2021-11-22 13:18:49 +0000

    DEV: Stub must extend ObjectImpl

--- yoko-rmi-spec/src/main/java/javax/rmi/CORBA/Stub.java
+++ yoko-rmi-spec/src/main/java/javax/rmi/CORBA/Stub.java
@@ -33,50 +33,40 @@ import java.security.PrivilegedExceptionAction;
 
 import static java.security.AccessController.doPrivileged;
 
-/**
- * This class is deliberately not serializable.
- * Its fields are never serialized.
- * When a serializable child class is constructed or deserialized,
- * this class's no-args constructor will be called.
- * This ensures the following:
- * <ul>
- *     <li>every child instance always has a delegate</li>
- *     <li>the delegate is never serialized</li>
- *     <li>the delegate is initialized even on deserialization.</li>
- * </ul>
- */
-abstract class DelegateHolder extends ObjectImpl {
-    private static final Constructor<? extends StubDelegate> DELEGATE_CONSTRUCTOR;
-
-    static {
-        try {
-            DELEGATE_CONSTRUCTOR = doPrivEx(DelegateType.STUB.getConstructorAction());
-        } catch (Exception e) {
-            throw (INITIALIZE) new INITIALIZE("Can not create Stub delegate").initCause(e);
-        }
-    }
-
-    protected final StubDelegate delegate;
-
-    /** This constructor must be public so that child classes can be deserialized successfully */
-    public DelegateHolder() {
-        try {
-            delegate = DELEGATE_CONSTRUCTOR.newInstance();
-        } catch (Exception e) {
-            throw (INITIALIZE) new INITIALIZE("Can not create Stub delegate").initCause(e);
-        }
-    }
-
-    private static <T> T doPrivEx(PrivilegedExceptionAction<T> action) throws PrivilegedActionException { return doPrivileged(action); }
-}
-
-public abstract class Stub extends DelegateHolder implements Serializable {
+public abstract class Stub extends ObjectImpl implements Serializable {
     static final long serialVersionUID = 1087775603798577179L;
+    private transient StubDelegate delegate = DelegateHelper.createDelegate();
     public void connect(ORB orb) throws RemoteException { delegate.connect(this, orb); }
-    private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException { delegate.readObject(this, ois); }
+    private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException {
+        delegate = DelegateHelper.createDelegate();
+        delegate.readObject(this, ois);
+    }
     private void writeObject(ObjectOutputStream oos) throws IOException { delegate.writeObject(this, oos); }
     @SuppressWarnings("EqualsWhichDoesntCheckParameterClass")
     public boolean equals(Object o) { return delegate.equals(this, o); }
     public int hashCode() { return delegate.hashCode(this); }
     public String toString() { return delegate.toString(this); }
+
+    enum DelegateHelper {
+        ;
+        private static final Constructor<? extends StubDelegate> DELEGATE_CONSTRUCTOR;
+
+        static {
+            try {
+                DELEGATE_CONSTRUCTOR = doPrivEx(DelegateType.STUB.getConstructorAction());
+            } catch (Exception e) {
+                throw (INITIALIZE) new INITIALIZE("Can not create Stub delegate").initCause(e);
+            }
+        }
+
+        private static StubDelegate createDelegate() {
+            try {
+                return DELEGATE_CONSTRUCTOR.newInstance();
+            } catch (Exception e) {
+                throw (INITIALIZE) new INITIALIZE("Can not create Stub delegate").initCause(e);
+            }
+        }
+
+        private static <T> T doPrivEx(PrivilegedExceptionAction<T> action) throws PrivilegedActionException { return doPrivileged(action); }
+    }
 }
