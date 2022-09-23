1a2e6a17a21ec3e441a3d9fac1b2ac51c62d0c10
Joe Chacko <143064+joe-chacko@users.noreply.github.com>
2022-09-23 23:59:20 +0100

    DEV: avoid processing fields for non-serializable classes

--- yoko-rmi-impl/src/main/java/org/apache/yoko/rmi/impl/ValueDescriptor.java
+++ yoko-rmi-impl/src/main/java/org/apache/yoko/rmi/impl/ValueDescriptor.java
@@ -298,7 +298,7 @@ class ValueDescriptor extends TypeDescriptor {
                 if (serial_persistent_fields == null) {
                     Field[] ff = type.getDeclaredFields();
 
-                    if ((ff == null) || (ff.length == 0)) {
+                    if ((!Serializable.class.isAssignableFrom(type)) || (ff == null) || (ff.length == 0)) {
                         _fields = new FieldDescriptor[0];
 
                     } else {
