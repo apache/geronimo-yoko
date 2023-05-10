512fba2de4792c0fe018cd75496aa6670be66cdf
habib <habiblawal@hotmail.co.uk>
2023-05-10 19:39:29 +0100

    DEV: Added mask to correctly convert signed byte to char

--- yoko-core/src/test/java-testify/org/apache/yoko/IdlStructTest.java
+++ yoko-core/src/test/java-testify/org/apache/yoko/IdlStructTest.java
@@ -1,26 +1,26 @@
-/*
- *  Licensed to the Apache Software Foundation (ASF) under one or more
- *  contributor license agreements.  See the NOTICE file distributed with
- *  this work for additional information regarding copyright ownership.
- *  The ASF licenses this file to You under the Apache License, Version 2.0
- *  (the "License"); you may not use this file except in compliance with
- *  the License.  You may obtain a copy of the License at
+/*==============================================================================
+ * Copyright 2023 IBM Corporation and others.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *=============================================================================*/
+
 package org.apache.yoko;
 
 import acme.Product;
 import acme.RemoteFunction;
 import org.junit.jupiter.api.Test;
-import testify.jupiter.annotation.iiop.ConfigureServer;
-import testify.jupiter.annotation.iiop.ConfigureServer.RemoteImpl;
+import testify.iiop.annotation.ConfigureServer;
+import testify.iiop.annotation.ConfigureServer.RemoteImpl;
 
 import java.rmi.RemoteException;
 
@@ -28,19 +28,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ConfigureServer
 public class IdlStructTest {
-    interface Discount extends RemoteFunction<Product, Product>{}
+    interface Register extends RemoteFunction<Product, Product>{}
+
+    private static final String REGISTERED_TRADEMARK = "\u00ae";
 
     @RemoteImpl
-    public static final Discount REMOTE = p-> new Product(p.name,p.price*0.9f);
+    public static final Register REMOTE = p -> new Product(p.name,p.price);
 
     @Test
-    public void sendProduct(Discount discount) throws RemoteException {
-        final String productName = "ORB-Buster\u00ae";
+    public void sendProduct(Register register) throws RemoteException {
+        final String productName = "ORB-Buster" + REGISTERED_TRADEMARK;
         System.out.println(productName);
-        Product result = discount.apply(new Product(productName, 50f));
-        assertEquals(45f, result.price, "String should be transmitted and received correctly");
+        Product result = register.apply(new Product(productName, 50f));
+        assertEquals(50f, result.price, "Float should be transmitted and received correctly");
         assertEquals(productName, result.name, "String should be transmitted and received correctly");
     }
-
-
 }
