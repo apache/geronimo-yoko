3790f2b4360d71bdfdec030ef1331404f91b7580
habib <habiblawal@hotmail.co.uk>
2023-07-28 12:45:14 +0100

    TST: refactored and ported POA deactivate test

--- yoko-core/src/test/java-testify/org/apache/yoko/orb/PortableServer/TestPoaDeactivate.java
+++ yoko-core/src/test/java-testify/org/apache/yoko/orb/PortableServer/TestPoaDeactivate.java
@@ -22,174 +22,84 @@ import org.omg.CORBA.OBJECT_NOT_EXIST;
 import org.omg.CORBA.ORB;
 import org.omg.CORBA.SystemException;
 import org.omg.PortableServer.POA;
-import org.omg.PortableServer.POAPackage.ObjectAlreadyActive;
-import org.omg.PortableServer.POAPackage.ObjectNotActive;
-import org.omg.PortableServer.POAPackage.ServantAlreadyActive;
-import org.omg.PortableServer.POAPackage.ServantNotActive;
-import org.omg.PortableServer.POAPackage.WrongPolicy;
 import test.poa.TestPOA;
-import test.poa.Test_impl;
 import testify.iiop.annotation.ConfigureOrb;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
-//
-// In this test we want to spawn a thread to call a method on the Test
-// interface. This method call should take some time. While the thread
-// is calling the method we attempt to deactivate the object. This
-// should not complete for some time, since it should wait for all
-// outstanding method calls to complete.
-//
+
 @ConfigureOrb
 public class TestPoaDeactivate {
     @Test
-    public void testDeactivate(ORB orb, POA rootPoa) {
-        Test_impl impl = new Test_impl(orb, "", false);
-
+    void testDeactivate(ORB orb, POA rootPoa) throws Exception {
+        test.poa.Test_impl impl = new test.poa.Test_impl(orb, "", false);
         test.poa.Test t = impl._this(orb);
-
         t.aMethod();
+        byte[] oid = rootPoa.servant_to_id(impl);
+        rootPoa.deactivate_object(oid);
+        assertThrows(OBJECT_NOT_EXIST.class, () -> t.aMethod());
+    }
 
-        byte[] oid = null;
-        try {
-            oid = rootPoa.servant_to_id(impl);
-        } catch (ServantNotActive ex) {
-            assertTrue(false);
-        } catch (WrongPolicy ex) {
-            assertTrue(false);
-        }
-
-        try {
-            rootPoa.deactivate_object(oid);
-        } catch (ObjectNotActive ex) {
-            assertTrue(false);
-        } catch (WrongPolicy ex) {
-            assertTrue(false);
-        }
-
-        try {
-            t.aMethod();
-            assertTrue(false); // expected OBJECT_NOT_EXIST
-        } catch (OBJECT_NOT_EXIST ex) {
-            // Expected
-        }
-        Test_impl2 impl1 = new Test_impl2(rootPoa);
-
-        test.poa.Test t1 = impl1._this(orb);
-
-        byte[] oid1 = null;
-        try {
-            oid1 = rootPoa.servant_to_id(impl1);
-        } catch (ServantNotActive ex) {
-            assertTrue(false);
-        } catch (WrongPolicy ex) {
-            assertTrue(false);
-        }
-
-        Thread thr = new LongCaller(t1);
-
+    // In this test we want to spawn a thread to call a method on the Test
+    // interface. This method call should take some time. While the thread
+    // is calling the method we attempt to deactivate the object. This
+    // should not complete for some time, since it should wait for all
+    // outstanding method calls to complete.
+    @Test
+    public void testDeactivateWaitsForMethodCompletion(ORB orb, POA rootPoa) throws Exception {
+        Test_impl impl = new Test_impl(rootPoa);
+        test.poa.Test t = impl._this(orb);
+        byte[] oid = rootPoa.servant_to_id(impl);
+        Thread thr = new LongCaller(t);
         thr.start();
-        impl1.blockUntilCalled();
-
-        //
+        impl.blockUntilCalled();
         // Test: deactivate_object while method call is active
-        //
-        try {
-            rootPoa.deactivate_object(oid1);
-        } catch (ObjectNotActive ex) {
-            assertTrue(false);
-        } catch (WrongPolicy ex) {
-            assertTrue(false);
-        }
+        rootPoa.deactivate_object(oid);
+        // Once we've deactivated the object the re-activation shouldn't complete until the method call completes
+        rootPoa.activate_object_with_id(oid, impl);
+        // The destroy call shouldn't return until the aMethod call is complete
+        assertTrue(impl.callComplete());
 
-        //
-        // Once we've deactivated the object the re-activation shouldn't
-        // complete until the method call completes
-        //
-        try {
-            rootPoa.activate_object_with_id(oid1, impl1);
-        } catch (ObjectAlreadyActive ex) {
-            assertTrue(false);
-        } catch (ServantAlreadyActive ex) {
-            assertTrue(false);
-        } catch (WrongPolicy ex) {
-            assertTrue(false);
-        }
-
-        //
-        // The destroy call shouldn't return until the aMethod call is
-        // complete
-        //
-        assertTrue(impl1.callComplete());
-
-        //
         // Wait for the thread to terminate
-        //
         while (thr.isAlive()) {
             try {
                 thr.join();
-            } catch (InterruptedException ex) {
+            } catch (InterruptedException ignored) {
             }
         }
-
-        try {
-            rootPoa.deactivate_object(oid1);
-        } catch (ObjectNotActive ex) {
-            assertTrue(false);
-        } catch (WrongPolicy ex) {
-            assertTrue(false);
-        }
+        rootPoa.deactivate_object(oid);
     }
 
-    final static class Test_impl2 extends TestPOA {
-        private POA poa_;
-
-        private boolean called_ = false;
-
-        private boolean finished_ = false;
-
-        Test_impl2(POA poa) {
-            poa_ = poa;
+    final static class Test_impl extends TestPOA {
+        private final POA poa;
+        private boolean called = false;
+        private boolean finished = false;
+        Test_impl(POA poa) {
+            this.poa = poa;
         }
 
         synchronized public void aMethod() {
-            called_ = true;
+            called = true;
             notify();
-
-            try {
-                wait(1000);
-            } catch (InterruptedException ex) {
-            }
-            finished_ = true;
+            try { wait(1000); } catch (InterruptedException ignored) {}
+            finished = true;
         }
 
-        public POA _default_POA() {
-            return poa_;
-        }
-
+        public POA _default_POA() { return poa; }
         synchronized void blockUntilCalled() {
-            while (!called_) {
-                try {
-                    wait();
-                } catch (InterruptedException ex) {
-                }
+            while (!called) {
+                try { wait(); } catch (InterruptedException ignored) {}
             }
         }
-
-        synchronized boolean callComplete() {
-            return finished_;
-        }
+        synchronized boolean callComplete() { return finished; }
     }
 
     final static class LongCaller extends Thread {
-        private test.poa.Test t_;
-
-        LongCaller(test.poa.Test t) {
-            t_ = t;
-        }
-
+        private final test.poa.Test test;
+        LongCaller(test.poa.Test test) { this.test = test; }
         public void run() {
             try {
-                t_.aMethod();
+                test.aMethod();
             } catch (SystemException ex) {
                 ex.printStackTrace();
             }
